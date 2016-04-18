a8020971f70fe5e8d7a02d499dfc2dab9ee443a5
xeodou <xeodou@gmail.com>
2016-04-19 01:03:18 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+## Webpack mill plugin
+
+This plugin is built for [Mill blog](https://graffie.github.io/mill/#/).
+
+
+## LICENSE 
+
+MIT
---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * webpack-mill-plugin - index.js
+ * Copyright(c) 2016 xeodou <xeodou@gmail.com>
+ * MIT Licensed
+ */
+
+'use strict';
+
+module.exports = requrire('./lib/MillPlugin');
